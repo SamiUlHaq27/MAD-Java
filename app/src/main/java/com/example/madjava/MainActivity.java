2b18259193319b8fb9d3dd1206d10df0df742a27
SamiUlHaq28 <22011598-068@uog.edu.pk>
2025-10-20 20:18:15 +0500

    life cycle functions

--- app/src/main/java/com/example/madjava/MainActivity.java
+++ app/src/main/java/com/example/madjava/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.madjava;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Toast.makeText(this, "this is onCreate Function", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onStart()
+    {
+        super.onStart();
+        Toast.makeText(this, "this is onStart Function", Toast.LENGTH_SHORT).show();
+
+    }
+
+    protected void onResume()
+    {
+        super.onResume();
+        Toast.makeText(this, "this is onResume Function", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onPause()
+    {
+        super.onPause();
+        Toast.makeText(this, "this is onPause Function", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onStop()
+    {
+        super.onStop();
+        Toast.makeText(this, "this is onStop Function", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Toast.makeText(this, "this is onDestroy Function", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onRestart()
+    {
+        super.onRestart();
+        Toast.makeText(this, "this is onRestart Function", Toast.LENGTH_SHORT).show();
     }
 }
+
